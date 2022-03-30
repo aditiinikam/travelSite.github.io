a74edcd2ac2f1759b0af001a4334497df7638dcd
Aditi Nikam <66727880+aditiinikam@users.noreply.github.com>
2022-03-30 21:48:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.querySelector("header").o
+// document.querySelector("h1").addClass("effects");
+
+// document.querySelector(".header").addEventListener("onmouseover", function(){
+//     document.querySelector("h1").style.visibility="visible";
+// });
+
+// $("").on("mouseover", function(){
+//   $("h1").addClass("effects");
+//   $("img").addClass("img-effect");
+// });
